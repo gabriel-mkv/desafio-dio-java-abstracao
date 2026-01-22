efa5b2cfe07fb26edda00cae5a8830186f0938f6
Gabriel Henrique <gabrielhsmendonca@gmail.com>
2026-01-21 21:08:08 -0300

    chore: adiciona estrutura base do projeto

---
+++ src/br/com/gabrielmkv/challengeDIO/Main.java
@@ -0,0 +1,9 @@
+package br.com.gabrielmkv.challengeDIO;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Método principal criado!");
+    }
+
+}
